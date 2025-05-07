8dde164e799e01edd674affea0ad3c9ca3b0547d
MacroHEX <martin18py@gmail.com>
2025-05-07 09:59:49 -0300

    First Commit

---
+++ src/test/java/net/macrohex/cineapi/CineapiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.macrohex.cineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
